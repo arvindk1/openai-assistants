49bd690e565dfd6be3bf25b07abf19c18ff8e625
arvindk1 <arvindsjunk@gmail.com>
2024-04-30 23:58:28 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import OpenAIAssistant from "@/app/ui/openai-assistant";
 
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-blue-500 to-indigo-600 py-12">
-      <div className="mx-auto max-w-lg text-center">
+    <main>
+      <div className="mx-auto mb-12 max-w-lg text-center">
         <div className="m-4">
-          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-white md:text-6xl lg:text-7xl">
-            InvestMate - Mutual Fund Advisor
-          </h1>
-          <div className="mb-8 text-xl font-normal text-white">
-            Where AI meets the financial industry
+          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">InvestMate - Mutual Fund Advisor</h1>
+          <div className="mb-6 text-normal font-normal text-gray-500">
+            Where AI meets the financial industry 
           </div>
         </div>
-        <div className="rounded-lg bg-white p-6 shadow-xl">
-          <OpenAIAssistant
-            assistantId="asst_XtZ2mLekWrWI7yVDtEKOFWV3"
-            greeting="Welcome to InvestMate! Would you like to explore our list of mutual funds, or do you have specific investment goals in mind for creating a diversified portfolio? I am a helpful chat assistant. How can I help you?"
-          />
-        </div>
+        <OpenAIAssistant 
+          assistantId="asst_XtZ2mLekWrWI7yVDtEKOFWV3"
+          greeting="Welcome to InvestMate! Would you like to explore our list of mutual funds, or do you have specific investment goals in mind for creating a diversified portfolio? I am a helpful chat assistant. How can I help you?"
+        />
       </div>
     </main>
   );
