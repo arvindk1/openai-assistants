cc0b4f08790143f8fe1631ebbf2754e285f997c6
arvindk1 <arvindsjunk@gmail.com>
2024-04-30 23:31:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
 import OpenAIAssistant from "@/app/ui/openai-assistant";
 
-
 export default function Home() {
   return (
-    <main>
-      <div className="mx-auto mb-12 max-w-lg text-center">
+    <main className="min-h-screen bg-gradient-to-b from-blue-500 to-indigo-600 py-12">
+      <div className="mx-auto max-w-lg text-center">
         <div className="m-4">
-          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">OpenAI Assistant Starter Kit</h1>
-          <div className="mb-6 text-normal font-normal text-gray-500">
-            This Starter Kit illustrates how you can create an OpenAI Assistant with ReactJS + NextJS + TypeScript.
-            You can <a href="https://github.com/Superexpert/openai-assistant-starter-kit" target="_new" className="underline text-blue-500">download the source code</a> for 
-            this Starter Kit from GitHub. 
+          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-white md:text-6xl lg:text-7xl">
+            InvestMate - Mutual Fund Advisor
+          </h1>
+          <div className="mb-8 text-xl font-normal text-white">
+            Where AI meets the financial industry
           </div>
         </div>
-        <OpenAIAssistant 
-          assistantId="asst_XtZ2mLekWrWI7yVDtEKOFWV3"
-          greeting="Welcome to InvestMate! Would you like to explore our list of mutual funds, or do you have specific investment goals in mind for creating a diversified portfolio? I am a helpful chat assistant. How can I help you?"
-        />
+        <div className="rounded-lg bg-white p-6 shadow-xl">
+          <OpenAIAssistant
+            assistantId="asst_XtZ2mLekWrWI7yVDtEKOFWV3"
+            greeting="Welcome to InvestMate! Would you like to explore our list of mutual funds, or do you have specific investment goals in mind for creating a diversified portfolio? I am a helpful chat assistant. How can I help you?"
+          />
+        </div>
       </div>
     </main>
   );
